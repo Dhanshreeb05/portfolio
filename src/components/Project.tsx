cd75eda98be192166793ed17b807e01bb09556bb
Dhanshreeb05 <44304377+Dhanshreeb05@users.noreply.github.com>
2025-03-24 17:14:55 -0400

    Updated projects part

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,13 +9,13 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://github.com/ashrane111/AI-GovBot" target="_blank" rel="noreferrer"><img src={harvey} className="zoom" alt="thumbnail" width="70%"/></a>
+                <a href="https://github.com/ashrane111/AI-GovBot" target="_blank" rel="noreferrer"><img src={harvey} className="zoom project-image" alt="thumbnail" width="70%"/></a>
                 <a href="https://github.com/ashrane111/AI-GovBot" target="_blank" rel="noreferrer"><h2>HARVEY</h2></a>
                 <p>Developed a chatbot for legal advice using RAG, Langchain, Airflow, MLFlow, Tensorflow.</p>
             </div>
             <div className="project">
-                <a href="https://github.com/ashrane111/AI-GovBot" target="_blank" rel="noreferrer"><img src={hermony} className="zoom" alt="thumbnail" width="70%"/></a>
-                <a href="https://github.com/ashrane111/AI-GovBot" target="_blank" rel="noreferrer"><h2>Hermony</h2></a>
+                <a href="https://github.com/hrishikasamani/Hermony" target="_blank" rel="noreferrer"><img src={hermony} className="zoom project-image" alt="thumbnail" width="70%"/></a>
+                <a href="https://github.com/hrishikasamani/Hermony" target="_blank" rel="noreferrer"><h2>Hermony</h2></a>
                 <p>Developed a platform helps women in tech harmonize their professional and personal lives through smart scheduling, mentorship, and community support.</p>
             </div>
         </div>
