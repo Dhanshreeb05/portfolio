115440e1109ed4b32224693fa7548ee693c21930
Dhanshreeb05 <44304377+Dhanshreeb05@users.noreply.github.com>
2025-03-29 00:52:42 -0400

    Added new projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import harvey from '../assets/images/chatbot.jpg';
 import hermony from '../assets/images/Hermony.png';
+import imgCraft from '../assets/images/imageCraft.jpg';
+import stockUp from '../assets/images/StockUp.jpg';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -17,6 +19,16 @@ function Project() {
                 <a href="https://github.com/hrishikasamani/Hermony" target="_blank" rel="noreferrer"><img src={hermony} className="zoom project-image" alt="thumbnail" width="70%"/></a>
                 <a href="https://github.com/hrishikasamani/Hermony" target="_blank" rel="noreferrer"><h2>Hermony</h2></a>
                 <p>Developed a platform helps women in tech harmonize their professional and personal lives through smart scheduling, mentorship, and community support.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/Dhanshreeb05/ImageProcessing" target="_blank" rel="noreferrer"><img src={imgCraft} className="zoom project-image" alt="thumbnail" width="70%"/></a>
+                <a href="https://github.com/Dhanshreeb05/ImageProcessing" target="_blank" rel="noreferrer"><h2>ImageCraft</h2></a>
+                <p>Developed an Image Processing Application in Java and Swing to perform various operations like crop, darken, brighten, greyscale and conversion, channel separation, etc on images.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/Dhanshreeb05/StockUp" target="_blank" rel="noreferrer"><img src={stockUp} className="zoom project-image" alt="thumbnail" width="70%"/></a>
+                <a href="https://github.com/Dhanshreeb05/StockUp" target="_blank" rel="noreferrer"><h2>StockUp</h2></a>
+                <p>A comprehensive stock trading and portfolio management application offering real-time market data, user authentication, transaction management, watchlists, and detailed financial insights.</p>
             </div>
         </div>
     </div>
